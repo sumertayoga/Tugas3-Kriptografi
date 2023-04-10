1af2cb65437676c1a5ea3dabe809e668d5d1d910
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2023-04-10 11:34:03 +0700

    add k-9 to repository

---
+++ k-9-main/app/ui/legacy/src/release/java/com/fsck/k9/ui/settings/NewSetupUiHack.kt
@@ -0,0 +1,8 @@
+package com.fsck.k9.ui.settings
+
+import com.fsck.k9.ui.settings.SettingsListFragment.SettingsListBuilder
+
+@Deprecated("Remove this once we switch over to the new setup UI")
+internal object NewSetupUiHack {
+    fun addAction(builder: SettingsListBuilder) = Unit
+}
